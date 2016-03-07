e7fbfcabd953a9be0292ffbe5d500c005c7993d3
Daniel <daniel.barinak@gmail.com>
2016-03-07 22:37:34 +0100

    Dont be mad if you find alot.of mistakes! XD wrote it sleepy

---
+++ lostteam/clearchat/cc.php
@@ -0,0 +1,41 @@
+namespace lostteam\clearchat;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\utils\TextFormat;
+
+class cc extends PluginBase implements Listener {
+
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("[ClearChat] Enabling...");
+    $this->getLogger()->info("[ClearChat] Enabled");
+  }
+
+  public function onPlayerChat(PlayerChatEvent $e){
+
+  $msg = $event->getMessage();
+
+   $cursewords = array("fuck","fu","ck","fuc","uck","fu*k","f*ck",
+"shit","poop","stupid,"dumb,"noob","anal","anus","arse","ass","ballsack","bastard","bitch","biatch","blowjob","blow job","boner"",boob","butt","buttplug","cock","crap","cunt","dick","dildo"," dyke","Goddamn","God damn","homo","jerk","nigger","nigga","omg","penis","piss","pussy","queer","scrotum","sex","s hit","sh1t","slut","vagina","wank","whore");
+
+
+   $advertwords = array("leet","cc",".leet","leet.",".cc","a1","a2","a3","a4","a5","a6","a7","a8","a9","b1","b2","b3","b4","b5","b6","b7","b8","b9","c1","c2","c3","c4","c5","c6","c7","c8","c9","192");
+
+   foreach ($this->cursewords as $word) {
+  if (strpos($this->cursewords, $word) !== false){
+    $event->setCancelled();
+   $event->getEntity()->kick(TextColor::RED."Do not Curse!", false);
+
+ }
+
+}
+ foreach ($this->advertwords as $word) {
+  if (strpos($this->advertwords, $word) !== false){
+    $event->setCancelled();
+    $event->getEntity()->kick(TextFormat::RED."Do not Advertise!", false);
+  }
+}
+  }
+}
